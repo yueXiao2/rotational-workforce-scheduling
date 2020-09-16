9249f92115add729bdcb5d919d59cfc2b6121e90
yueXiao2 <yue.xiao1@outlook.com>
2020-09-16 16:44:21 +1000

    decomposition on the paper

---
+++ decomposition.py
@@ -0,0 +1,41 @@
+from gurobipy import *
+from fileReader import *
+
+file = "testcases/Example"
+num = 100
+
+dataMap = read_data(file+str(num)+".txt")
+
+planningLength = dataMap['scheduleLength']
+G = range(planningLength)
+
+numEmployee = dataMap['numEmployees']
+E = range(numEmployee)
+
+schedulingLength = planningLength * numEmployee
+D = range(schedulingLength)
+
+shiftType = []
+for day in ['morning', 'afternoon', 'night']:
+    if dataMap[day] != []:
+        shiftType.append(dataMap[day][0])
+S = range(dataMap['numShifts'])
+
+workDemand = dataMap['matrix']
+
+minD = dataMap['minDaysOffLength']
+maxD = dataMap['maxDaysOffLength']
+minWork = dataMap['minWorkBlockLength']
+maxWork = dataMap['maxWorkBlockLength']
+minShift = []
+maxShift = []
+for day in ['morning', 'afternoon', 'night']:
+    if dataMap[day] != []:
+        minShift.append(dataMap[day][1])
+        maxShift.append(dataMap[day][2])
+
+F2 = dataMap['notAllowedShiftSequences2']
+
+F3 = dataMap['notAllowedShiftSequences3']
+
+m = Model('RWS')
