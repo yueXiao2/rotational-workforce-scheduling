5ad653210523117d8110d74e5e11bc7f7e3295f0
yueXiao2 <yue.xiao1@outlook.com>
2020-09-22 11:58:43 +1000

    completed master problem

--- decomposition.py
+++ decomposition.py
@@ -15,19 +15,25 @@ E = range(numEmployee)
 schedulingLength = planningLength * numEmployee
 D = range(schedulingLength)
 
+# S = ['D','A','N']
 shiftType = []
 for day in ['morning', 'afternoon', 'night']:
     if dataMap[day] != []:
         shiftType.append(dataMap[day][0])
 S = range(dataMap['numShifts'])
 
+# T[s][d]
 workDemand = dataMap['matrix']
 
 minD = dataMap['minDaysOffLength']
 maxD = dataMap['maxDaysOffLength']
 minWork = dataMap['minWorkBlockLength']
 maxWork = dataMap['maxWorkBlockLength']
+
+# e.g.[1,2,3]
 minShift = []
+
+# e.g.[1,2,3]
 maxShift = []
 for day in ['morning', 'afternoon', 'night']:
     if dataMap[day] != []:
@@ -47,7 +53,8 @@ DW = range(maxWork)
 L = minWork
 
 while L <= maxWork:
-    m = Model('RWS')
+    m = Model('computing feasible shift blocks')
+    m.setParam('OutputFlag', 0)
     
     X = {(s,d):m.addVar(vtype = GRB.BINARY) for s in S for d in DW}
     Y= {(s):m.addVar(vtype = GRB.BINARY) for s in S}
@@ -84,8 +91,7 @@ while L <= maxWork:
                     b.append(s)
         
         b = tuple(b)
-    
-        print(b)
+
     
 
         B.append(b)
@@ -93,9 +99,48 @@ while L <= maxWork:
     else:
         L = L + 1
                 
-print("Solutions found")
+print("feasible blocks found")
+
+#Computes the converage of a shift block b starting on day d
+#return: list of shifts s in day g that are covered
+def Coverage(b,d):
+    currentDay = d
+    coverageList = []
+    
+    for i in b:
+        tupleList = tuple([i,currentDay])
+        coverageList.append(tupleList)
+        currentDay = (currentDay + 1)%planningLength
+    
+    return coverageList
+
+#given the shift s in day d that is needed to be covered
+#return the shift block b staring on day g that is able to cover this
+def C(s,d):
+    CList = []
+    for g in G:
+        for b in range(len(B)):
+            if (s,d) in Coverage(B[b],g):
+                CList.append(tuple([b,g]))
+    return CList
 
 
+m = Model("master")
+m.setParam('OutputFlag',0)
+
+#number of times that shift block b starts from day d
+X = {(b,d):m.addVar(vtype = GRB.INTEGER) for d in G for b in range(len(B))}
+
+
+#sum of blocks b that start from day g that can provide coverage on shift s and day d must equal to the demand s and d. 
+OnShiftDemand = {(s,d):m.addConstr( quicksum(X[b,g] for (b,g) in C(s,d)) == workDemand[s][d]) for s in S for d in G}
+m.optimize()
+print("covers found")
+
+for d in G:
+    for b in range(len(B)):
+        if X[b,d].x > 0.9:
+            print('block ' + str(b) + ' starts ' + str(X[b,d].x) + ' times from day ' + str(d))
 
 
 
